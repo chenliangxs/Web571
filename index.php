ae64c51e94f31924e0714c056c783860d5be51dc
LiangChen <chenliangxs@gmail.com>
2017-08-12 09:41:28 -0700

    Android files

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__ . '/php-graph-sdk-5.0.0/src/Facebook/autoload.php';$my_access_token='EAAZAaNfCxTeUBAPECymzeMuMVtNhxBZCKUZBhanOIe26PGAaWsmj1kHzlQeQMC6j6KSYYMXPdHKJw3kYBsitNPX6zXXsUlMsImosZBSEG98SRHXriaFqIrkjRgI0toKLXBLBxhHyFJLpc35ftLzdrnCbaLWsOC8ZD';$fb = new Facebook\Facebook(['app_id' => '1788037578182117','app_secret'=>'5c84d6cfed7a61551dbe38cf5b7ffe8a','default_graph_version' => 'v2.8',]);$fb->setDefaultAccessToken($my_access_token);
+<?php require_once __DIR__ . '/php-graph-sdk-5.0.0/src/Facebook/autoload.php';$my_access_token='EAAZAaNfCxTeUBABf9zWrez30BZAeaJbAYgdCwQzVpnmaa2AinTA1xjNHmVjZC6OxwncRCwKdiyBizXGvFk7wMayvBA7Pnh9x130JomGlSg8YUHyQIbZBgIt8ft0TR2R24TvaGKcDv1jLsNraPbJRd5FEsQ7IGpgZD';$fb = new Facebook\Facebook(['app_id' => '1788037578182117','app_secret'=>'5c84d6cfed7a61551dbe38cf5b7ffe8a','default_graph_version' => 'v2.8',]);$fb->setDefaultAccessToken($my_access_token);
   
 if(isset($_GET["keyword"])){
     $keyword=$_GET["keyword"];
@@ -9,10 +9,10 @@ $types=array("user","page","event","place","group");
 echo "{";
 foreach($types as $type){
    if($type=="place"){
-       $searchURLUser='https://graph.facebook.com/v2.8/search?q='.$keyword.'&type='.$type.'&center='.$latitude.','.$longitude.'&distance='.'1000'.'&fields=id,name,picture.width(700).height(700)&access_token='.$my_access_token;
+       $searchURLUser='https://graph.facebook.com/v2.8/search?limit=10&q='.$keyword.'&type='.$type.'&center='.$latitude.','.$longitude.'&distance='.'1000'.'&fields=id,name,picture.width(700).height(700)&access_token='.$my_access_token;
    }
     else{
-        $searchURLUser='https://graph.facebook.com/v2.8/search?q='.$keyword.'&type='.$type.'&fields=id,name,picture.width(700).height(700)&access_token='.$my_access_token;
+        $searchURLUser='https://graph.facebook.com/v2.8/search?limit=10&q='.$keyword.'&type='.$type.'&fields=id,name,picture.width(700).height(700)&access_token='.$my_access_token;
     }
     $response=file_get_contents($searchURLUser);
     echo "\"".$type."\"".":".$response;
